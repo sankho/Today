13ccd96b02f1c7e6ef3160c7110c76d2a91a2ca1
Sankho Mallik <sankho.mallik@gmail.com>
2011-09-20 23:13:17 -0400

    some updates

--- public/js/mylibs/todo.basemodel.js
+++ public/js/mylibs/todo.basemodel.js
@@ -12,7 +12,7 @@ TODO.baseModel = function() {
             var collection = TODO.clientDB.getCollection(this.collection);
 
             if (!this.doc._id) {
-                // might want to find something better than sha256. shorter, even.
+                // might want to find something other than sha256. shorter, even.
                 this.doc._id = 'new_' + Sha256.hash(Math.floor(Math.random(1)*999));
                 // probably should check if this ID exists elsewhere first, meh
             }
